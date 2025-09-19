7abea7fe09b219f40a097fbdf88fec20bb40abba
diana ndinda <52344898+afrochic@users.noreply.github.com>
2025-09-19 10:46:52 +0300

    Update route.ts

--- app/api/attendee/verify/route.ts
+++ app/api/attendee/verify/route.ts
@@ -1,5 +1,6 @@
 import { type NextRequest, NextResponse } from "next/server"
 import { getUserByEmail } from "@/lib/db"
+export const dynamic = "force-dynamic";
 
 export async function GET(request: NextRequest) {
   try {
